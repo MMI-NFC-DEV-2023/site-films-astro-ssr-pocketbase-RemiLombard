ed3992ffc5d75dbb2ab30c77ff3266917758ce0a
Rémi-Lombard <remi.lombard@edu.univ-fcomte.fr>
2025-02-18 13:40:50 +0100

    mise à jour de la configuration de PocketBase pour utiliser l'URL depuis les variables d'environnement

--- src/backend.ts
+++ src/backend.ts
@@ -1,4 +1,6 @@
 import PocketBase from 'pocketbase'
 import type { TypedPocketBase } from './pocketbase-typegen'
 
-export const pb = new PocketBase("https://astro-ssr.remilombard.fr/") as TypedPocketBase
+export const pb = new PocketBase(
+  import.meta.env.VITE_URL_POCKETBASE
+) as TypedPocketBase;
